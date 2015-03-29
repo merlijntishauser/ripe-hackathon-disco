4b092e3473f1c21f1cdf039f8f2a3f4c45e84001
Anna-Kaisa Pietilainen <annakaisa.pietilainen@gmail.com>
2015-03-29 12:43:16 +0200

    trending topics test

---
+++ twitterclitest/weather.js
@@ -0,0 +1,29 @@
+(function() {    
+    var map = L.map('map').setView([0,0],2);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var updatetweets = function(loc) {        
+        console.log(loc);
+        $.ajax({
+//            url: 'http://127.0.0.1:3000/trending?lat='+loc.lat+'&lng='+loc.lng,
+            url: 'http://127.0.0.1:3000/trending?country=NL',
+            success : function(res) {
+                console.log(res);
+            }
+        });
+    }
+
+    var update = function(event) {
+        var newloc = map.getCenter(); 
+        setTimeout(updatetweets,0,newloc);
+        // weather etc.
+    }
+
+    map.on('viewreset', update);
+    map.on('dragend', update);
+
+    // zoom to current loc (html5 geoloc)
+    map.locate({setView : true, maxZoom: 5});
+})();
