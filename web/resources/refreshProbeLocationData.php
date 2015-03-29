30c0db62b950cf32c61932425188068145d126a8
Merlijn Tishauser <merlijn@gargleblaster.org>
2015-03-29 13:47:35 +0200

    small code modifications

--- web/resources/refreshProbeLocationData.php
+++ web/resources/refreshProbeLocationData.php
@@ -3,12 +3,12 @@
 	$raw = file_get_contents("https://atlas.ripe.net/api/v1/probe-archive/?format=json");
 	$data = json_decode($raw, true);
 
-	$probes = array();
+	$probes = [];
 	foreach ($data['objects'] as $key => $value) {
-		$probe = array();
-		$probe['long'] = $value['longitude'];
-		$probe['lat'] = $value['latitude'];
-		$probes[(int)$value['id']] = $probe;
+		$probe = [];
+		$probe['long']              = $value['longitude'];
+		$probe['lat']               = $value['latitude'];
+		$probes[(int)$value['id']]  = $probe;
 	}
 
     file_put_contents(realpath(dirname(__FILE__)) . "/locations.json", json_encode($probes));
